165238a0958d8945076731955b6b7e3a2deb88fc
elif ece <eceeliftest@gmail.com>
2024-02-14 01:11:52 +0300

    Apod css eklemeleri 1

--- src/Apod.js
+++ src/Apod.js
@@ -1,15 +1,19 @@
 import React from "react";
+import "./Apod.css";
+import "./ApodTitle.css";
+import "./ApodDate.css";
+import "./ApodExplanation.css";
 
 const ApodComponent = (props) => {
   const { NasaData } = props;
 
   return (
     <div className="Apod">
-      
-      <h1>{NasaData.title}</h1>
-      <h2>{NasaData.date}</h2>
+      <h1 className="ApodTitle">{NasaData.title}</h1>
+      <h2 className="ApodDate">{NasaData.date}</h2>
       <img className="img" src={NasaData.url} alt={NasaData.title} />
-      <p>{NasaData.explanation}</p>
+      <p className="ApodExplanation">{NasaData.explanation}</p>
+      <img className="img2" src={NasaData.hurl} alt={NasaData.title}/>
     </div>
   );
 };
