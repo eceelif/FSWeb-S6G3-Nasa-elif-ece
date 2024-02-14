636f0b494a27ebd62933541356e2a364d4f25968
elif ece <eceeliftest@gmail.com>
2024-02-15 01:02:55 +0300

    kolon düzenlemesi yapıldı

--- src/NasaData.js
+++ src/NasaData.js
@@ -1,13 +1,13 @@
 const api_key = "eizXyyCxYnegNCb3xlTrOMggDwdkRruFkMw3lqUz";
 
 let ApiObj = {
-  fetchData: async function (inputValue) {
+  fetchData: async function (count) {
     var requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
 
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&count=${inputValue}`, requestOptions);
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&count=${count}`, requestOptions);
     let result = await response.json();
     return result;
   }
